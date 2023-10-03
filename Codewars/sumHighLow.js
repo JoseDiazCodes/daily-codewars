3e9873980ad59c44a110c0bfad218b9271fe341f
JoseDiazCodes <josediazjr.a@gmail.com>
2023-10-02 21:23:25 -0400

    added sumHighLow.js

---
+++ Codewars/sumHighLow.js
@@ -0,0 +1,4 @@
+function sumArray(array) {
+	// sorted the array, now im thinking about using a filter method to filter out the smallest and largest integer
+	let sortedArr = array.sort((a, b) => a - b);
+}
