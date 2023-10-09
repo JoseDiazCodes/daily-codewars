a53ceba3a1ba4c91dfacfd1223bc20100f690e9c
JoseDiazCodes <josediazjr.a@gmail.com>
2023-10-08 22:18:26 -0400

    added another alternative solution to recurrsiveFactorial.js

--- Codewars/recurrsiveFactorial.js
+++ Codewars/recurrsiveFactorial.js
@@ -11,3 +11,10 @@ function factorial(n) {
 	}
 	return n * factorial(n - 1);
 }
+
+// another way to solve the problem
+
+function factorial(n) {
+	if (n < 0 || n > 12) throw new RangeError();
+	return n <= 1 ? 1 : n * factorial(n - 1);
+}
