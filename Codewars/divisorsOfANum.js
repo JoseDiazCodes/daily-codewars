4c50cfdf2f580f35ff4ce130e3dbebec129f58ad
JoseDiazCodes <josediazjr.a@gmail.com>
2023-10-09 00:03:22 -0400

    added divisorsOfANum.js

--- Codewars/divisorsOfANum.js
+++ Codewars/divisorsOfANum.js
@@ -1,14 +1,8 @@
-// link: https://www.codewars.com/kata/542c0f198e077084c0000c2e/train/javascript
-//solution
 function getDivisorsCnt(n) {
 	let count = 0;
-
-	for (let i = 1; i * i <= n; i++) {
+	for (let i = 1; i <= n; i++) {
 		if (n % i === 0) {
 			count++;
-			if (i !== n / i) {
-				count++;
-			}
 		}
 	}
 	return count;
