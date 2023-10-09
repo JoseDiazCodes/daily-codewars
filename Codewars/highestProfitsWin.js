51b3271e04619abeae33d35cfa6681278fb1c9ef
JoseDiazCodes <josediazjr.a@gmail.com>
2023-10-08 22:33:33 -0400

    added highestProfitsWin.js

---
+++ Codewars/highestProfitsWin.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/559590633066759614000063/train/javascript
+
+// solution
+function minMax(arr) {
+	let min = Math.min(...arr);
+	let max = Math.max(...arr);
+
+	return [min, max];
+}
