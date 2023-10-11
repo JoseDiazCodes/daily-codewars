aca6f6edb63619db1d6d5aa19766eed8e448a409
JoseDiazCodes <josediazjr.a@gmail.com>
2023-10-11 14:18:15 -0400

    added another solution

--- Codewars/sqEveryDigit.js
+++ Codewars/sqEveryDigit.js
@@ -7,3 +7,11 @@ function squareDigits(num) {
 	let result = numberArray.map((e) => e * e).join("");
 	return Number(result);
 }
+
+// link to problem: https://www.codewars.com/kata/546e2562b03326a88e000020/train/javascript
+// solution
+// function squareDigits(num){
+// 	num = num.toString().split('').map(Number).map((num) => num * num).join('')
+
+// 	return Number(num);
+//   }
